bd32dc5c98b048b5179704fbaf8f09b41d87096e
gashema ibrahim <ibrahimgashema154@gmail.com>
2019-08-26 13:47:07 +0000

    six

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+    validates :mail, presence: true
+    validates :mail, length: {in: 1..140}
 end
--- db/migrate/20190821131943_create_tweets.rb
+++ db/migrate/20190821131943_create_tweets.rb
@@ -1,8 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.text :content
-
+      t.string :username
+      t.text :mail
       t.timestamps
     end
   end
